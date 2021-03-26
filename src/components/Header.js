e9056acf10dffea2f0cf294e98855c9a58063514
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 12:54:26 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -82,7 +82,7 @@ export default class Header extends React.Component {
   tincidunt."
                                 data-item-image="/images/rajmudi_rice_672x672.png"
                                 data-item-name="Rajmudi Rice">
-                                Add to cart dwebs
+                                Add to cart dwensil
                                 </button>
                                 <ul className="hamburger__nav">
                                     {_.map(_.get(site, 'siteMetadata.main_menu', null), (item, item_idx) => {
