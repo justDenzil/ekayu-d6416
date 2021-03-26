34319f83541a6ae068db0b54fbd72c54a253b645
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 06:57:20 +0000

    store.js: changed

--- src/templates/store.js
+++ src/templates/store.js
@@ -27,16 +27,7 @@ export default class Store extends React.Component {
                     )
                 })}
             </main>
-            <button
-    class="snipcart-add-item"
-    data-item-id="2"
-    data-item-name="Bacon"
-    data-item-price="3.00"
-    data-item-weight="20"
-    data-item-url="http://myapp.com/products/bacon"
-    data-item-description="Some fresh bacon">
-        Buy bacon
-</button>
+            
             </Layout>
         );
     }
