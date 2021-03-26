13649b01103775b39a235ced9c2b70b84adfa440
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 20:57:21 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,6 +68,8 @@ export default class Header extends React.Component {
                                         <path d="M22.682 6.079h-22.682v1.712h22.814l-4.574 4.528 1.194 1.182 6.566-6.5-6.566-6.5-1.194 1.182 4.442 4.397z" />
                                     </svg>
                                 </button>
+                                <span class="snipcart-items-count"></span>
+<span class="snipcart-total-price"></span>
                                 <ul className="hamburger__nav">
                                     {_.map(_.get(site, 'siteMetadata.main_menu', null), (item, item_idx) => {
                                         let section = _.get(page, 'frontmatter.section', null) || _.get(page, 'frontmatter.title', null);
